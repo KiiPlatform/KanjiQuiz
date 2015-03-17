147e2eb7c37a51e113c11358fa190393ab149493
Riza <riza.alaudin@kii.com>
2015-03-17 11:41:11 +0900

    Init projects.

---
+++ KanjiQuiz/AppLogic/Quiz.swift
@@ -0,0 +1,13 @@
+//
+//  Quiz.swift
+//  KanjiQuizz
+//
+//  Created by Syah Riza on 3/16/15.
+//  Copyright (c) 2015 Kii. All rights reserved.
+//
+
+import UIKit
+
+public class Quiz: NSObject {
+    
+}
---
+++ KanjiQuiz/KanjiQuiz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KanjiQuizz
+//
+//  Created by Syah Riza on 3/16/15.
+//  Copyright (c) 2015 Kii. All rights reserved.
+//
+
+import UIKit
+import AppLogic
+
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
