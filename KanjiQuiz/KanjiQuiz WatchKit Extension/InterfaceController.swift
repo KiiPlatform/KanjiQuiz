932593535e1bc753b46dd26815f4af054d7d6d3b
Riza <riza.alaudin@kii.com>
2015-04-02 15:02:32 +0900

    reindent.

--- KanjiQuiz/KanjiQuiz WatchKit Extension/InterfaceController.swift
+++ KanjiQuiz/KanjiQuiz WatchKit Extension/InterfaceController.swift
@@ -11,57 +11,57 @@ import Foundation
 import AppLogic
 
 class InterfaceController: WKInterfaceController {
+  
+  @IBOutlet weak var displayName: WKInterfaceLabel!
+  override func awakeWithContext(context: AnyObject?) {
+    super.awakeWithContext(context)
+    setupKii()
+    var val = userDisplayName() as String
     
-    @IBOutlet weak var displayName: WKInterfaceLabel!
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        setupKii()
-        var val = userDisplayName() as String
-        
-        self.displayName.setText(val)
-        
-        // Configure interface objects here.
-    }
-    func startQuiz(quiz: Quiz){
-        var contexts = Array<AnyObject>()
-        var pages = Array<AnyObject>()
-        
-        for (index,value) in enumerate(quiz.problems!) {
-            let cont = ["index":index,"problem":value]
-            contexts.append(cont)
-            pages.append("ProblemInterface")
-        }
-        
-        pages.append("ResultInterface")
-        
-        self.presentControllerWithNames(pages, contexts: contexts)
-    }
-    @IBAction func startSpellingQuiz() {
-        let quiz = Quiz(type: QuizType.Spelling, level: .N5)
-        quiz.setup()
-        setCurrentQuiz(quiz)
-        self.startQuiz(quiz)
-    }
-    @IBAction func startMeaningQuiz() {
-        let quiz = Quiz(type: QuizType.Meaning, level: .N5)
-        quiz.setup()
-        setCurrentQuiz(quiz)
-        self.startQuiz(quiz)
-        
-    }
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+    self.displayName.setText(val)
+    
+    // Configure interface objects here.
+  }
+  func startQuiz(quiz: Quiz){
+    var contexts = Array<AnyObject>()
+    var pages = Array<AnyObject>()
+    
+    for (index,value) in enumerate(quiz.problems!) {
+      let cont = ["index":index,"problem":value]
+      contexts.append(cont)
+      pages.append("ProblemInterface")
     }
     
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
+    pages.append("ResultInterface")
     
+    self.presentControllerWithNames(pages, contexts: contexts)
+  }
+  @IBAction func startSpellingQuiz() {
+    let quiz = Quiz(type: QuizType.Spelling, level: .N5)
+    quiz.setup()
+    setCurrentQuiz(quiz)
+    self.startQuiz(quiz)
+  }
+  @IBAction func startMeaningQuiz() {
+    let quiz = Quiz(type: QuizType.Meaning, level: .N5)
+    quiz.setup()
+    setCurrentQuiz(quiz)
+    self.startQuiz(quiz)
     
-    override func contextForSegueWithIdentifier(segueIdentifier: String) -> AnyObject? {
-        println(segueIdentifier)
-        return nil
-    }
+  }
+  override func willActivate() {
+    // This method is called when watch view controller is about to be visible to user
+    super.willActivate()
+  }
+  
+  override func didDeactivate() {
+    // This method is called when watch view controller is no longer visible
+    super.didDeactivate()
+  }
+  
+  
+  override func contextForSegueWithIdentifier(segueIdentifier: String) -> AnyObject? {
+    println(segueIdentifier)
+    return nil
+  }
 }
